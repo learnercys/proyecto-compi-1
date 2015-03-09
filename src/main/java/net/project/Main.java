23da90335196bcb207e3c617795ac3f6760f4c43
Carlos Hernández <learner.cys@gmail.com>
2015-03-09 01:03:18 -0600

    initial commit. Building structures.

---
+++ src/main/java/net/project/Main.java
@@ -0,0 +1,30 @@
+package net.project;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+/**
+ * @author learnercys
+ * Created on 3/9/15.
+ */
+public class Main extends Application{
+    public static final String APP_TITLE = "";
+    public static java.net.URL appBase;
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // defining base route.
+        appBase = getClass().getResource("");
+
+        Pane root = FXMLLoader.load( getClass().getResource("fxml/mainctrl.fxml") );
+        primaryStage.setTitle( APP_TITLE );
+        primaryStage.setScene( new Scene( root ) );
+        primaryStage.show();
+    }
+
+    public static void main( String[] args ) {
+        launch( args );
+    }
+}
---
+++ src/main/java/net/project/controllers/MainCtrl.java
@@ -0,0 +1,8 @@
+package net.project.controllers;
+
+/**
+ * @author learnercys [learner.cys@gmail.com]
+ */
+public class MainCtrl {
+}
+
